fb7a7681051b8eeba0a6d39d4edefcf924dd6fb7
atipchao <atip.chaogunhar@cookcountyil.gov>
2023-11-12 16:02:48 -0600

    Add project files.

---
+++ BlazorWasnAppNet6NoneAuth/Server/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorWasnAppNet6NoneAuth.Shared.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWasnAppNet6NoneAuth.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+            
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ BlazorWasnAppNet6NoneAuth/Shared/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasnAppNet6NoneAuth.Shared.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
